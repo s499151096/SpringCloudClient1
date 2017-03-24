21fee021f9af2b5d080bcaa3d69a1ef2c05e7aa6
s499151096 <499151096@qq.com>
2017-03-24 14:13:45 +0800

    Initial commit

---
+++ src/main/java/com/bcht/springcloud/client/SpringCloudClientApplication.java
@@ -0,0 +1,14 @@
+package com.bcht.springcloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientApplication.class, args);
+	}
+}
